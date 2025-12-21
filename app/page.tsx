695b01c18c65e395995947de829e64f8ac3e7608
ankanZtoH <ankan06edu@gmail.com>
2025-12-21 15:10:22 +0530

    file

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen text-center p-8 bg-black/40 backdrop-blur-[3px]">
-      <h1 className="absolute top-20 left-40  text-8xl md:text-8xl font-black mb-6 tracking-tighter text-white drop-shadow-2xl">
+    <div className="flex flex-col items-start justify-between min-h-screen text-center p-8 bg-black/40 backdrop-blur-[3px]">
+      <h1 className="text-8xl md:text-8xl font-black ml-30 mt-10 tracking-tighter text-white drop-shadow-2xl">
         PERCEPTRON
       </h1>
 
@@ -12,8 +12,8 @@ export default function Home() {
         Crack the code. <br className="hidden md:block" />
         Unlock the door.
       </h1> */}
-      <Link href="/puzzle" passHref className="absolute bottom-30 left-90 ">
-        <button className="group relative px-8 py-4 bg-yellow-400 text-black font-bold text-xl rounded-full overflow-hidden transition-transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-yellow-400 focus:ring-offset-2 focus:ring-offset-black">
+      <Link href="/puzzle" passHref>
+        <button className="group relative ml-80 mb-20 px-8 py-4 bg-yellow-400 text-black font-bold text-xl rounded-full overflow-hidden transition-transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-yellow-400 focus:ring-offset-2 focus:ring-offset-black">
           <span className="relative z-10 group-hover:text-black transition-colors cursor-pointer">Let's Go</span>
         </button>
       </Link>
